83ae185e1daad1966d38fa943390e187bc0651b2
半田優太 <yuta.h5221@gmail.com>
2024-07-27 16:07:20 +0900

    second

---
+++ typescript-type/index.ts
@@ -0,0 +1,19 @@
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = 'hello';
+let double: string = "hello";
+let back: string = `hello`;
+const person = {
+    name: {
+        first: 'Jack',
+        second: 'Smith'
+    }, 
+    age: 21
+} 
+const fruits: any[] = ['Apple', 'Banana', 'Grape', 1]
+const book: [string, number, boolean] = ['business', 1500, false];
+book[1] = 700;
+book.push(21);
+console.log(person);
